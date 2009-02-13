9044c7148d68c97095f06625554df2d4029d9455
Walker Hamilton <walker@walker-hamiltons-macbook-pro.local>
2009-02-13 13:23:34 -0600

    Cleaned up back-end interface - added lock, aligned inputs and delete/move graphics, removed unneeded js function to change margins

--- system/modules/helmsman/js/scripts.js
+++ system/modules/helmsman/js/scripts.js
@@ -23,7 +23,6 @@ jQuery(document).ready(function() {
 	bindDelete();
 	bindDeleteHover();
 	bindSortable();
-	resetMargin();
 });
 
 function bindInputKeypresses() {
@@ -63,11 +62,6 @@ function bindSortable() {
 						}
 						// console.log(depth);
 						jQuery(this).children('div.hidden').children('input').attr('value', depth);
-						amt = (120-(depth*20))
-						amt = amt.toString();
-						marg = amt+'px';
-						jQuery(this).children('input.leftmost').css('margin-left', marg);
-						//$("#" + this).text("My id is " + this + ".");
 					});
 				}
 			});
@@ -96,11 +90,6 @@ function bindSortable() {
 					}
 					// console.log(depth);
 					jQuery(this).children('div.hidden').children('input').attr('value', depth);
-					amt = (120-(depth*20))
-					amt = amt.toString();
-					marg = amt+'px';
-					jQuery(this).children('input.leftmost').css('margin-left', marg);
-					//$("#" + this).text("My id is " + this + ".");
 				});
 			}
 		});
@@ -120,29 +109,5 @@ function bindDelete() {
 	jQuery('a.delete-link').click(function() {
 		jQuery(this).parent().remove();
 		// console.log(jQuery(this).parent().html());
-	});
-}
-
-function resetMargin() {
-	jQuery.each(jQuery('#master-list li'), function() {
-		var anid = jQuery(this).parent()[0].id;
-		var counter = 0;
-		while(anid!='master-list') {
-			counter++;
-			num_parents = '.parent()';
-			for(i=1;i<counter;i++) { num_parents += '.parent()'; }
-			the_next = "jQuery(this)"+num_parents+"[0].id";
-			// console.log(the_next);
-			anid = eval(the_next);
-			var depth = ((i-1)/2)+1;
-		}
-		if(typeof(depth)=='undefined')
-		{
-			var depth = 1;
-		}
-		amt = (120-(depth*20))
-		amt = amt.toString();
-		marg = amt+'px';
-		jQuery(this).children('input.leftmost').css('margin-left', marg);
 	});
 }
