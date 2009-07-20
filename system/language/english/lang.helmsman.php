3d480526eb2d8b5762517e76e9843f3e2358583b
Walker Hamilton <walker@walker-hamiltons-macbook-pro.local>
2009-07-20 15:33:41 -0500

    Removed old version for 1.6.7 EE

--- system/language/english/lang.helmsman.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$L = array(
-		"helmsman_module_name" => "Helmsman",
-		"helmsman_module_description" => "Create and manage a complex navigation schema.",
-		"helmsman_save" => "Save",
-	);
-
-?>
